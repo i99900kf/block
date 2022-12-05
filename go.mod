52b63db5bec3f7128638a1daf9c8acf62128c216
Eric <857355787@qq.com>
2022-12-05 15:25:33 +0800

    day3

--- go.mod
+++ go.mod
@@ -1,3 +1,8 @@
 module block
 
 go 1.16
+
+require (
+	github.com/boltdb/bolt v1.3.1 // indirect
+	golang.org/x/sys v0.3.0 // indirect
+)
--- main.go
+++ main.go
@@ -1,16 +1,15 @@
 package main
 
 import (
-	"block/blockchain2"
+	"block/blockchain3"
 	"fmt"
-	"strconv"
 )
 
 /*
  * @Author: YJH
  * @Date: 2022-11-29 18:35:50
  * @LastEditors: YJH
- * @LastEditTime: 2022-12-02 10:34:28
+ * @LastEditTime: 2022-12-05 15:02:25
  * @FilePath: \go-crmf:\docker\block\main.go
  * @Description:
  *
@@ -18,18 +17,26 @@ import (
  */
 func main() {
 	//bc:=blockchain1.NewBlockchain()/第一版：通过包名称来调用
-	bc := blockchain2.NewBlockchain() //第二版：通过包名称来调用
-	bc.AddBlock("Send 1 BTC to Ivan.")
-	bc.AddBlock("Send 2 more BTC to Ivan.")
-	for _, block := range bc.Blocks {
-		fmt.Printf("Prev.hash:%x\n", block.PrevBlockHash)
-		fmt.Printf("Data:%s\n", block.Data)
-		fmt.Printf("Hash:%x\n", block.Hash)
-		fmt.Printf("Nonce:%d八n", block.Nonce) //第二版
-		//验证pow:第二版
-		pow := blockchain2.NewProofOfWork(block)
-		fmt.Printf("POW:%s\n", strconv.FormatBool(pow.Validate()))
-		//打印空行：自动换行(Printf不能自动换行)
-		fmt.Println()
-	}
+	// bc := blockchain2.NewBlockchain() //第二版：通过包名称来调用
+	// bc.AddBlock("Send 1 BTC to Ivan.")
+	// bc.AddBlock("Send 2 more BTC to Ivan.")
+	// for _, block := range bc.Blocks {
+	// 	fmt.Printf("Prev.hash:%x\n", block.PrevBlockHash)
+	// 	fmt.Printf("Data:%s\n", block.Data)
+	// 	fmt.Printf("Hash:%x\n", block.Hash)
+	// 	fmt.Printf("Nonce:%d八n", block.Nonce) //第二版
+	// 	//验证pow:第二版
+	// 	pow := blockchain2.NewProofOfWork(block)
+	// 	fmt.Printf("POW:%s\n", strconv.FormatBool(pow.Validate()))
+	// 	//打印空行：自动换行(Printf不能自动换行)
+	// 	fmt.Println()
+	// }
+	bc := blockchain3.NewBlockchain() //第三版
+	fmt.Println(bc)
+	defer bc.Db.Close()
+	//key:value的方式构建struct实例是更严谨的写法
+	//如果是在包内使用，直接写value是可以的
+	//如果在包外使用，直接写value会出错：composite literal uses unkeyed fields
+	cli := blockchain3.CLI{BC: bc}
+	cli.Run()
 }
